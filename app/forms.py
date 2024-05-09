385b707b890d24885323def8fb527d4ac7e2a999
pablojosecodes <pablojrhansen@gmail.com>
2024-05-08 21:41:03 -0700

    functional authentication:

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,32 @@
+from app import db
+from app.models import User
+
+from sqlalchemy import select
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, URLField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, URLField, EmailField
+from wtforms.validators import DataRequired, ValidationError
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    email = EmailField("Password", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    about = TextAreaField("About Me")
+    remember_me = BooleanField("Remember Me?")
+    submit = SubmitField("Sign In")
+    
+    
+    def validate_username(self, username):
+        user = db.session.scalar(select(User).where(
+            User.username == username.data))
+        if user is not None:
+            raise ValidationError(('Please use a different username.'))
+
+    def validate_email(self, email):
+        user = db.session.scalar(select(User).where(
+            User.email == email.data))
+        if user is not None:
+            raise ValidationError(('Please use a different email address.'))
+
 
 
 class LoginForm(FlaskForm):
--- app/main/routes.py
+++ app/main/routes.py
@@ -3,37 +3,54 @@ from flask import url_for
 from flask import render_template, flash, redirect
 from app import app
 from app import db
-from app.forms import LoginForm, RecommendForm
+from app.forms import LoginForm, RecommendForm, RegistrationForm
 from flask import request, url_for, abort
 
-from flask_login import login_user
+from flask_login import login_user, current_user, logout_user
 import sqlalchemy as sa
 from app.models import User
 from app.main import bp
 
+
+
 @bp.route("/")
 @bp.route("/index")
-def index():
-    user = {'username': 'Miguel'}
-    return render_template('index.html', title='Home', user=user, active_page="home", testing=True)
-
 @bp.route("/home")
 def home():
     user = {'username': 'Miguel'}
-    return render_template('home.html', user=user)
+    return render_template('home.html', user=current_user)
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    # form = LoginForm()
+    # if form.validate_on_submit():
+    #     user = db.session.scalar(
+    #         sa.select(User).where(User.username == form.username.data))
+    #     if user is None or not user.check_password(form.password.data):
+    #         flash(_('Invalid username or password'))
+    #         return redirect(url_for('auth.login'))
+    #     login_user(user, remember=form.remember_me.data)
+    #     next_page = request.args.get('next')
+    #     if not next_page or urlsplit(next_page).netloc != '':
+    #         next_page = url_for('main.home')
+    #     return redirect(next_page)
+    # return render_template('auth/login.html', title=_('Sign In'), form=form)
+
+    # if current_user.is_authenticated:
+    #     return redirect(url_for('main.home'))
+
     form = LoginForm()
     if form.validate_on_submit():
         user = db.session.scalar(sa.select(User).where(User.username == form.username.data))
-        if user is None or user.check_password(form.username.password):
+        if user is None or not user.check_password(form.password.data):
             flash("Invalid login. Try again.")
-            redirect(url_for("main.login"))
-        login_user(user)
+            return redirect(url_for("main.login"))
+        login_user(user, remember=form.remember_me.data)
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect(url_for('main.index'))
+        return redirect(url_for('main.home'))
     return render_template('forms/login.html', title='Sign In', form=form)
 
 @bp.route('/feedback', methods=['GET', 'POST'])
@@ -41,8 +58,15 @@ def feedback():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Feedback submitted!')
-        return redirect(url_for('main.index'))
+        return redirect(url_for('main.home'))
     return render_template('forms/feeedback.html', title='Feedback', form=form)
+
+
+@bp.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main.home'))
+
 
 
 @bp.route("/recommend", methods=["GET", "POST"])
@@ -53,3 +77,20 @@ def recommend():
 
         return redirect(url_for("recommend") +"?success=True")
     return render_template('forms/recommend.html', title="Recommend Podcast", form=form)
+
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(('Congratulations, you are now a registered user!'))
+        return redirect(url_for('main.login'))
+    return render_template('forms/register.html', title=('Register'),
+                           form=form)
+
